6364019c124e5b36eb2dae5b2fd6e55ee322efcd
0721Betty <1942433767@qq.com>
2024-03-20 09:59:29 +0800

    feat(utils): add useBatchSettings & downloadFile

---
+++ packages/utils/lib/__test__/useBatchSettings.test.ts
@@ -0,0 +1,199 @@
+import { describe, it, expect, vi } from 'vitest'
+import useBatchSettings from '../useBatchSettings'
+
+describe('useBatchSettings', () => {
+  const {
+    filenameMap,
+    templateContentMap,
+    handleDownloadTemp,
+    readFileAndParse,
+    processIoTDBData,
+    processTDengineData,
+    processInfluxDBData,
+  } = useBatchSettings()
+
+  describe('filenameMap', () => {
+    it('should have the correct format', () => {
+      Object.keys(filenameMap).forEach((key) => {
+        expect(typeof key).toBe('string')
+        expect(typeof filenameMap[key]).toBe('string')
+      })
+    })
+  })
+
+  describe('templateContentMap', () => {
+    it('should have the correct format', () => {
+      Object.keys(templateContentMap).forEach((key) => {
+        expect(typeof key).toBe('string')
+        expect(typeof templateContentMap[key]).toBe('string')
+      })
+    })
+  })
+
+  describe('handleDownloadTemp', () => {
+    it('should generate correct blob', () => {
+      const blob = new Blob(['test template'], { type: 'text/csv' })
+      const fakeURL = 'http://fakeurl.com/blob'
+      const createObjectURL = vi.fn(() => fakeURL)
+      const revokeObjectURL = vi.fn(() => {})
+
+      window.URL.createObjectURL = createObjectURL
+      window.URL.revokeObjectURL = revokeObjectURL
+
+      handleDownloadTemp('test template', 'test.csv')
+      expect(createObjectURL).toHaveBeenCalledWith(blob)
+      expect(revokeObjectURL).toHaveBeenCalledWith(fakeURL)
+    })
+
+    it('should log an error if the template is empty', () => {
+      console.error = vi.fn()
+      handleDownloadTemp()
+      expect(console.error).toHaveBeenCalledWith('Template is empty')
+    })
+  })
+
+  describe('readFileAndParse', () => {
+    it('should parse CSV file correctly', async () => {
+      const file = new File(['1,2,3\n4,5,6'], 'test.csv', { type: 'text/csv' })
+      const maxRows = 10
+      const expectedData = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+      ]
+
+      const result = await readFileAndParse(file, maxRows)
+      expect(result).toEqual(expectedData)
+    })
+
+    it('should reject with an error if failed to parse CSV data', async () => {
+      const file = new File(['Name,Age\nJohn,30\n,40\nAlice'], 'test.csv', { type: 'text/csv' })
+      const maxRows = 10
+
+      const result = readFileAndParse(file, maxRows)
+      await expect(result).rejects.toThrow('Failed to parse CSV data')
+    })
+
+    it('should reject with an error if the number of rows exceeds the maximum limit', async () => {
+      const file = new File(['1,2,3\n4,5,6\n7,8,9'], 'test.csv', { type: 'text/csv' })
+      const maxRows = 1
+
+      const result = readFileAndParse(file, maxRows)
+      await expect(result).rejects.toThrow(
+        'The number of rows in the CSV file exceeds the limit. Up to 1 rows of data are supported except for the header',
+      )
+    })
+  })
+
+  describe('processIoTDBData', () => {
+    it('should process IoTDB data correctly', async () => {
+      const data = [
+        ['timestamp', 'measurement', 'data_type', 'value'],
+        ['2022-01-01', 'temperature', 'FLOAT', '25.5'],
+        ['2022-01-02', 'humidity', 'INT32', '60'],
+      ]
+      const expectedOutput = [
+        {
+          timestamp: '2022-01-01',
+          measurement: 'temperature',
+          data_type: 'FLOAT',
+          value: '25.5',
+        },
+        {
+          timestamp: '2022-01-02',
+          measurement: 'humidity',
+          data_type: 'INT32',
+          value: '60',
+        },
+      ]
+
+      const result = await processIoTDBData(data)
+      expect(result).toEqual(expectedOutput)
+    })
+
+    it('should throw an error for invalid data type', async () => {
+      const data = [
+        ['timestamp', 'measurement', 'data_type', 'value'],
+        ['2022-01-01', 'temperature', 'INVALID', '25.5'],
+      ]
+
+      await expect(processIoTDBData(data)).rejects.toThrow('Invalid data type: INVALID')
+    })
+  })
+
+  describe('processTDengineData', () => {
+    it('should process TDengine data correctly', async () => {
+      const data = [
+        ['field', 'value', 'isChar'],
+        ['field1', 'value1', 'true'],
+        ['field2', 'value2', 'false'],
+      ]
+      const expectedOutput = "insert into <table>(field1, field2) values ('value1', value2)"
+
+      const result = await processTDengineData(data)
+      expect(result).toEqual(expectedOutput)
+    })
+
+    it('should skip empty field or value', async () => {
+      const data = [
+        ['field', 'value', 'isChar'],
+        ['', 'value1', 'true'],
+        ['field1', '', 'true'],
+        ['field2', 'value2', 'false'],
+      ]
+      const expectedOutput = 'insert into <table>(field2) values (value2)'
+
+      const result = await processTDengineData(data)
+      expect(result).toEqual(expectedOutput)
+    })
+
+    it('should throw an error for invalid isChar flag', async () => {
+      const data = [
+        ['field', 'value', 'isChar'],
+        ['field1', 'value1', 'invalid'],
+      ]
+
+      await expect(processTDengineData(data)).rejects.toThrow('Invalid Char Value field: invalid')
+    })
+  })
+
+  describe('processInfluxDBData', () => {
+    it('should process InfluxDB data correctly', async () => {
+      const data = [
+        ['key', 'value'],
+        ['temperature', '25.5'],
+        ['humidity', '60'],
+      ]
+      const expectedOutput = [
+        { key: 'temperature', value: '25.5' },
+        { key: 'humidity', value: '60' },
+      ]
+
+      const result = await processInfluxDBData(data)
+      expect(result).toEqual(expectedOutput)
+    })
+
+    it('should skip rows with missing key or value', async () => {
+      const data = [
+        ['key', 'value'],
+        ['', '25.5'],
+        ['humidity', ''],
+        ['pressure', '30'],
+      ]
+      const expectedOutput = [{ key: 'pressure', value: '30' }]
+
+      const result = await processInfluxDBData(data)
+      expect(result).toEqual(expectedOutput)
+    })
+
+    it('should catch error', async () => {
+      const badData = [['key1', 'value1'], null]
+
+      try {
+        // @ts-ignore
+        await processInfluxDBData(badData)
+      } catch (error) {
+        expect(error).toBeTruthy()
+      }
+    })
+  })
+})
---
+++ packages/utils/lib/useBatchSettings.ts
@@ -0,0 +1,214 @@
+import Papa from 'papaparse'
+import { createI18n } from 'vue-i18n'
+import { createDownloadBlobLink } from './download'
+
+enum BatchSettingTypes {
+  IoTDB = 'iotdb',
+  TDengine = 'tdengine',
+  InfluxDB = 'influxdb',
+}
+
+export default (locale: 'zh' | 'en' = 'en') => {
+  const i18n = createI18n({
+    locale,
+    messages: {
+      en: {
+        iotdbTemplateRemark:
+          'Measurement, Value, and Data Type are required fields. The Data Type can have the optional values BOOLEAN, INT32, INT64, FLOAT, DOUBLE, TEXT.',
+        invalidIsCharFlag: 'Invalid Char Value field: {isChar}',
+        uploadMaxRowsError:
+          'The number of rows in the CSV file exceeds the limit. Up to {max} rows of data are supported except for the header',
+        influxdbTemplateRemark:
+          'Append an i to the field value to tell InfluxDB to store the number as an integer.',
+      },
+      zh: {
+        iotdbTemplateRemark:
+          '字段、值、数据类型是必填选项，数据类型可选的值为 BOOLEAN、INT32、INT64、FLOAT、DOUBLE、TEXT',
+        invalidIsCharFlag: '无效的字符标识符值：{isChar}',
+        uploadMaxRowsError: 'CSV 文件行数超过限制，除表头外，最多支持 {max} 行数据',
+        influxdbTemplateRemark: '在字段值后追加 i，InfluxDB 则将该数值存储为整数类型。',
+      },
+    },
+  })
+  const { t } = i18n.global
+
+  const filenameMap = {
+    [BatchSettingTypes.InfluxDB]: 'InfluxDB',
+    [BatchSettingTypes.TDengine]: 'TDengine',
+    [BatchSettingTypes.IoTDB]: 'IoTDB',
+  }
+
+  const templateContentMap = {
+    [BatchSettingTypes.TDengine]: `Field,Value,Char Value,Remarks (Optional)
+ts,now,FALSE,Example Remark
+msgid,\${id},TRUE,
+mqtt_topic,\${topic},TRUE,
+qos,\${qos},FALSE,
+temp,\${payload.temp},FALSE,
+hum,\${payload.hum},FALSE,
+status,\${payload.status},FALSE,
+`,
+    [BatchSettingTypes.IoTDB]: `Timestamp,Measurement,Data Type,Value,Remarks (Optional)
+now,temp,FLOAT,\${payload.temp},"${t('iotdbTemplateRemark')}"
+now,hum,FLOAT,\${payload.hum},
+now,status,BOOLEAN,\${payload.status},
+now,clientid,TEXT,\${clientid},
+`,
+    [BatchSettingTypes.InfluxDB]: `Field,Value,Remarks (Optional)
+temp,\${payload.temp},
+hum,\${payload.hum},
+precip,\${payload.precip}i,"${t('influxdbTemplateRemark')}"
+`,
+  }
+
+  /**
+   * Processes TDengine data and returns a promise that resolves to a string.
+   *
+   * @param {string[][]} data - The TDengine data to be processed.
+   * @returns {Promise<string>} - A promise that resolves to the generated SQL insert string.
+   */
+  const processTDengineData = (data: string[][]): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      try {
+        const tableName = '<table>'
+        const fields = []
+        const values = []
+        for (let i = 1; i < data.length; i++) {
+          const [field, value, isChar] = data[i]
+          if (!field || !value) {
+            continue
+          }
+          fields.push(field)
+          const isCharValue = ['true', 'TRUE', '1', '', undefined].includes(isChar?.trim())
+          const isNotCharValue = ['false', 'FALSE', '0'].includes(isChar?.trim())
+          if (isCharValue) {
+            values.push(`'${value}'`)
+          } else if (isNotCharValue) {
+            values.push(value)
+          } else {
+            throw new Error(t('invalidIsCharFlag', { isChar }))
+          }
+        }
+        const result = `insert into ${tableName}(${fields.join(', ')}) values (${values.join(
+          ', ',
+        )})`
+        resolve(result)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  /**
+   * Processes IoTDB data and returns an array of records.
+   * @param {string[][]} data - The IoTDB data to be processed.
+   * @returns {Promise<Array<Record<string, any>>>} - A promise that resolves to an array of records.
+   * @throws {Error} - If an invalid data type is encountered.
+   */
+  const processIoTDBData = (data: string[][]): Promise<Array<Record<string, any>>> => {
+    return new Promise((resolve, reject) => {
+      try {
+        const validDataTypes = ['BOOLEAN', 'INT32', 'INT64', 'FLOAT', 'DOUBLE', 'TEXT']
+
+        const result = data
+          .slice(1)
+          .filter(
+            (row) => row.length >= 4 && row.slice(0, 4).every((item) => item && item.trim() !== ''),
+          )
+          .map((row) => {
+            const [timestamp, measurement, data_type, value] = row
+            // Check if data_type is valid
+            if (!validDataTypes.includes(data_type)) {
+              throw new Error(`Invalid data type: ${data_type}`)
+            }
+            return {
+              timestamp,
+              measurement,
+              data_type,
+              value,
+            }
+          })
+
+        resolve(result)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  /**
+   * Processes the InfluxDB data and returns a promise that resolves to an array of key-value pairs.
+   * @param {string[][]} data - The InfluxDB data to be processed.
+   * @returns {Promise<{ key: string; value: string }[]>} - A promise that resolves to an array of key-value pairs.
+   */
+  const processInfluxDBData = (data: string[][]): Promise<{ key: string; value: string }[]> => {
+    return new Promise((resolve, reject) => {
+      try {
+        // Skip the first row and map each row to an object
+        const result = [] as { key: string; value: string }[]
+        for (let i = 1; i < data.length; i++) {
+          const [key, value] = data[i]
+          if (!key || !value) {
+            continue
+          }
+          result.push({ key, value })
+        }
+        resolve(result)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  const handleDownloadTemp = (template?: string, filename?: string) => {
+    if (template) {
+      const blob = new Blob([template], { type: 'text/csv' })
+      createDownloadBlobLink(blob, `${filename}_template.csv`)
+    } else {
+      console.error('Template is empty')
+    }
+  }
+
+  /**
+   * Reads and parses a CSV file.
+   * @param file The file to be read and parsed.
+   * @returns A promise that resolves to a 2D array representing the CSV data.
+   */
+  const readFileAndParse = async (file: File, maxRows: number): Promise<string[][]> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        if (!event.target) {
+          return reject(new Error('FileReader event target is null'))
+        }
+        const csvData = event.target.result
+        const results: any = Papa.parse(csvData as string, { skipEmptyLines: 'greedy' })
+        if (results.errors.length > 0) {
+          reject(new Error('Failed to parse CSV data: ' + results.errors[0].message))
+        }
+        if (results.data.length > maxRows + 1) {
+          reject(new Error(t('uploadMaxRowsError', { max: maxRows })))
+        } else {
+          resolve(results.data)
+        }
+      }
+      reader.onerror = () => {
+        reject(new Error('An error occurred while reading the file'))
+      }
+      reader.onabort = () => {
+        reject(new Error('File reading was aborted'))
+      }
+      reader.readAsText(file)
+    })
+  }
+
+  return {
+    filenameMap,
+    templateContentMap,
+    handleDownloadTemp,
+    readFileAndParse,
+    processIoTDBData,
+    processTDengineData,
+    processInfluxDBData,
+  }
+}
