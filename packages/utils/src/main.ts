b5e0cdca32a19e17ead7ab39635c966f0b6a2e35
0721Betty <1942433767@qq.com>
2025-06-24 14:45:39 +0800

    feat(utils): add rule func & field json files & useRuleFunc hook

--- packages/utils/src/main.ts
+++ packages/utils/src/main.ts
@@ -1,7 +1,10 @@
-import { formatSizeUnit, flattenObject, unflattenObject } from '../lib'
+import { formatSizeUnit, flattenObject, unflattenObject, RuleFunc, RuleField } from '../lib'
 
 console.log(formatSizeUnit(12969))
 
 console.log('Output of flattenObject function: ', flattenObject({ a: { b: { c: 1 } } }))
 
 console.log('Output of unflattenObject function: ', unflattenObject({ 'a.b.c': 1 }))
+
+console.log('RuleFunc: ', RuleFunc)
+console.log('RuleField: ', RuleField)
